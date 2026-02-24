8928e00777848a56ff71382dd68d5009fc959248
Ishi-1512 <ishitamittal1512@gmail.com>
2026-02-24 10:26:39 +0530

    Javascript lab 1

---
+++ Javascript/jsintro.js
@@ -0,0 +1,14 @@
+/* Javascript is a cross platform,object-based scriptinf language for client and server application,it is used everywhere 
+ run in just in time compilation
+losely typeed language
+can be embedded within html
+ high level and multi paradigm
+js features
+ extends functionaly of web page
+ written in the html document
+controls page Element
+reacts to user actions
+js/java is not similar 
+expression= ends with ";"
+*/
+
